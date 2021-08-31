69130a9aaef48999801f6a579313ba8a47f692e0
Andrei Duicu <andreiduicu@192-168-0-246.rdsnet.ro>
2021-08-31 15:22:02 +0300

    add project

---
+++ app/Http/Controllers/CladireController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cladire;
+use Illuminate\Http\Request;
+
+class CladireController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cladire  $cladire
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cladire $cladire)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cladire  $cladire
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cladire $cladire)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cladire  $cladire
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cladire $cladire)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cladire  $cladire
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cladire $cladire)
+    {
+        //
+    }
+}

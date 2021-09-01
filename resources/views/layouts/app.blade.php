cd99b3fb8420a8027e78e93d615da3741fba3323
BlueVortex123 <duicu.andrei@gmail.com>
2021-09-01 08:17:33 +0000

    add views logic

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Complex Cladiri</title>
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="h-100"> 
+
+    <main class="py-4 h-100">
+        @yield('content')
+    </main>
+    
+</body>
+</html>
